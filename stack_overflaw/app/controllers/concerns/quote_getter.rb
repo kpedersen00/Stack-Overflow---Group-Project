22413ef8e2b928ec3b905543ab39be1c77d91165
Josh Croff & Kelsey Pedersen <pair+joshuacroff.kpedersen00@devbootcamp.com>
2015-06-10 11:58:19 -0700

    Add Github API key for release 4

--- stack_overflaw/app/controllers/concerns/quote_getter.rb
+++ stack_overflaw/app/controllers/concerns/quote_getter.rb
@@ -2,7 +2,7 @@ module QuoteGetter
   include HTTParty
 
   def quote
-    @response = HTTParty.get('https://api.github.com/zen',  :basic_auth => {:username => 'joshuacroff', :password => 'Bonobo231'})
+    @response = HTTParty.get('https://api.github.com/zen',  :basic_auth => ENV['API_KEY'])
     @quotes = ["Knock, knock", "Josh and Kelsey are awesome and are killing this challenge!"]
     ratelimit = @response.headers["x-ratelimit-remaining"].to_i
     if ratelimit > 0
